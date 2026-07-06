183dc722d8354b3aa5c8ea7db30abc49c4945b29
NTT-CKN <23130333@st.hcmuaf.edu.vn>
2026-07-07 00:31:25 +0700

    ui Customer

---
+++ CUSTOMER/JS/login.js
@@ -0,0 +1,13 @@
+const loginForm = document.querySelector('form');
+
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if(username && password) {
+        alert('Đăng nhập thành công!');
+        window.location.href = 'home.html';
+    }
+});
